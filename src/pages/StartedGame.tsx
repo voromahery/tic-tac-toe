9d542d2cd84f61597bc4822500aef6952be7f071
voromahery <daniel.her@gmail.org>
2021-09-13 17:31:48 +0300

    Adds an item in each cells

--- src/pages/StartedGame.tsx
+++ src/pages/StartedGame.tsx
@@ -1,14 +1,12 @@
 import React, {useEffect, useState} from "react";
 import {useAppSelector, useAppDispatch} from '../app/hooks';
 import {newPlayer} from '../features/addPlayer'
-// import { newSecondPlayer} from '../features/addSecondPlayer'
+import { newSecondPlayer} from '../features/addSecondPlayer'
 import {gameTimer} from '../features/timer'
- import {start} from '../features/startScreenSlice'
-// 0 | 1 | 2
-// ---+---+---
-//  3 | 4 | 5
-// ---+---+---
-//  6 | 7 | 8 
+import {start} from '../features/startScreenSlice'
+import cross from '../images/cross.svg'
+import circle from '../images/circle.svg'
+
 // Possibility to win
 //[0, 1, 2]
 //[3, 4, 5]
@@ -20,18 +18,60 @@ import {gameTimer} from '../features/timer'
 
 //[6, 4, 2]
 //[0, 4, 8]
+
 export default function StartedGame(){
     const dispatch = useAppDispatch();
     const firstPlayer = useAppSelector(newPlayer);
-    // const secondPlayer = useAppSelector(newSecondPlayer);
+    const secondPlayer = useAppSelector(newSecondPlayer);
     const timer = useAppSelector(gameTimer)
     const [countDown, setCountDown] = useState(timer)
+    const circlePiece = <img src={circle} alt='circle'/>
+    const crossPiece = <img src={cross} alt='cross'/>
+    const [squares, setSquares] = React.useState(Array(9).fill(null))
+    const [player, setPlayer] = useState(firstPlayer)
+
+    function calculateNextValue(squares:any) {
+        return squares.filter(Boolean).length % 2 === 0 ? crossPiece : circlePiece
+      }
+      
+useEffect(() => {
+    if(squares.filter(Boolean).length % 2 === 0){
+        setPlayer(firstPlayer)
+    } else {
+        setPlayer(secondPlayer)
+    }
+}, [player])
+
+    const nextValue = calculateNextValue(squares)
+    // const winner = calculateWinner(squares)
+    // const status = calculateStatus(winner, squares, nextValue)
+  
+    function selectSquare(square:any) {
+    //   if (winner || squares[square]) {
+    //     return
+    //   }
+      const squaresCopy = [...squares]
+      squaresCopy[square] = nextValue
+      setSquares(squaresCopy)
+    }
+  
+    // function restart() {
+    //   setSquares(Array(9).fill(null))
+    // }
+  
+    function renderSquare(i:any) {
+        console.log(squares)
+      return (
+        <button className='cell' onClick={() => selectSquare(i)}>
+          {squares[i]}
+        </button>
+      )
+    }
 
    useEffect(()=>{
     let myInterval = setInterval(() => {
             countDown > 0 && setCountDown(countDown - 1)
         }, 1000)
-        
         return ()=> {
             clearInterval(myInterval);
           };
@@ -40,21 +80,21 @@ export default function StartedGame(){
 
     return(
 <div className='started-wrapper'>
-    <p className='player-to-play'>{firstPlayer}’s turn</p>
+    <p className='player-to-play'>{player}’s turn</p>
 <div className='board-container'>
             <div className="vertical-border left"></div>
-            <div data-cell-index="0" className="cell 0"></div>
-            <div data-cell-index="1" className="cell 1"></div>
-            <div data-cell-index="2" className="cell 2"></div>
             <div className="horizontal-border top"></div>
-            <div data-cell-index="3" className="cell 3"></div>
-            <div data-cell-index="4" className="cell 4"></div>
-            <div data-cell-index="5" className="cell 5"></div>
             <div className="horizontal-border down"></div>
-            <div data-cell-index="6" className="cell 6"></div>
-            <div data-cell-index="7" className="cell 7"></div>
-            <div data-cell-index="8" className="cell 8"></div>
             <div className="vertical-border right"></div>
+            {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
         </div>
         {countDown > 0 ? <p className='remaining-time'>time left: {countDown}s</p> : <button onClick={() => dispatch(start())} className='start-button'>Restart</button>}
 </div>
